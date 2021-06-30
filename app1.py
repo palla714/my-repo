4fb9e03d9b572d96ba15226fdbcb6d6ea64b4e5c
Pallavi Das <das.pallavi355@gmail.com>
2021-07-01 01:01:16 +0530

    new modification

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ name = input('Enter the weekday')
 if (name == 'Monday'or name == 'Tuesday' or name == 'wednesday' or name == 'Thursday' or name == 'Friday'):
     print ("9 to 5:30")
 elif (name == "saturday"):
-    print ("9 to 1")
+    print ("10 to 1")
 
 else:
    print("Holiday")
